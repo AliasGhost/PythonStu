22e0161731d174bf240de07dc896cc7eeb6867bd
shenyg <shenyg2412@gamil.com>
2025-03-28 14:24:10 +0800

    day-3

--- LeeCode/day-3.py
+++ LeeCode/day-3.py
@@ -8,33 +8,3 @@
 @Desc      : 
 """
 
-from collections import deque
-
-
-class MyQueue:
-    def __init__(self):
-        self.queue = deque()
-
-    def push(self, x: int) -> None:
-        self.queue.append(x)  # [1, 2, 3]
-        for _ in range(len(self.queue) - 1):
-            self.queue.append(self.queue.popleft())  # [2, 3, 1][3, 1, 2]
-
-    def pop(self) -> int:
-        return self.queue.popleft()
-
-    def top(self) -> int:
-        return self.queue[0]
-
-    def empty(self) -> bool:
-        return len(self.queue) == 0
-
-
-if __name__ == '__main__':
-    q = MyQueue()
-    q.push(1)
-    q.push(2)
-    q.push(3)
-    print(q.top())
-    print(q.pop())
-    print(q.empty())
